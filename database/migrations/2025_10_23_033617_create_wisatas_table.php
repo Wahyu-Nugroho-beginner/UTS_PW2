1c5877a08d3c75812d27604f6b3613de8d708063
Yozxora <albatinnopriayadi8899@gmail.com>
2025-10-23 12:07:43 +0700

    nambah database

--- database/migrations/2025_10_23_033617_create_wisatas_table.php
+++ database/migrations/2025_10_23_033617_create_wisatas_table.php
@@ -11,17 +11,48 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('wisata', function (Blueprint $table) {
+        Schema::create('login_users', function (Blueprint $table) {
             $table->id();
+            $table->string('email', 100)->unique();
+            $table->string('username', 50);
+            $table->string('password', 50);
+            $table->timestamps();
+        });
+
+        Schema::create('produks', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_produk', 100);
+            $table->integer('jumlah_produk');
+            $table->integer('harga');
+            $table->timestamps();
+        });
+
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->id('id_rating');
+            $table->foreignId('id_user')->constrained('login_users')->onDelete('cascade');
+            $table->string('nama', 50);
+            $table->text('komentar');
+            $table->integer('rating');
+            $table->timestamps();
+        });
+
+        Schema::create('pesanans', function (Blueprint $table) {
+            $table->id('id_pesanan');
+            $table->foreignId('id_user')->constrained('login_users')->onDelete('cascade');
+            $table->foreignId('id_produk')->constrained('produks')->onDelete('cascade');
+            $table->integer('jumlah_pesanan');
+            $table->integer('total_harga');
+            $table->string('nama_produk', 50);
+            $table->date('tanggal_pesanan');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('wisata');
+        Schema::dropIfExists('pesanans');
+        Schema::dropIfExists('ratings');
+        Schema::dropIfExists('produks');
+        Schema::dropIfExists('login_users');
     }
 };
