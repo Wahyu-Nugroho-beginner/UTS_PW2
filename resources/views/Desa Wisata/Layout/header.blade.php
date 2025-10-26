5e3ca6de804f1a6365ba671fc2b175f75f30413e
ridra116 <ridrapratama2@gmail.com>
2025-10-26 18:31:21 +0700

    Add .gitattributes to normalize line endings

---
+++ resources/views/Desa Wisata/Layout/header.blade.php
@@ -0,0 +1,4 @@
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMMvLxMFLQvwsUvpUjSGnbmQGzgnGUKxw-lQ&s" alt="" class="text-center">
+<br>
+<a href="{{ route('dosen') }}" class="btn btn-primary">Lihat Data</a>
+<a href="{{ route('tambahDosen') }}" class="btn btn-warning">Input Data</a>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('kerinci');
 });
 
 Route::resource('wisata', WisataController::class);
+
+// Simple page route for Danau Kerinci
+Route::get('/kerinci', function () {
+    return view('kerinci');
+});
