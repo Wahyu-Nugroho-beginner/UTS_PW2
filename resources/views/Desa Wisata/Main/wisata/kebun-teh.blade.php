f11ef438dd1ef00472fb56d6223bac89abe59ece
Wahyu Nugroho <triwahyunugroho78@gmail.com>
2025-11-04 20:45:58 +0700

    hapus semua file yang ada head

--- resources/views/Desa Wisata/Main/wisata/kebun-teh.blade.php
+++ resources/views/Desa Wisata/Main/wisata/kebun-teh.blade.php
@@ -1,18 +1,8 @@
 @extends('desa wisata.layout.master')
 
-@section('title', 'Halaman Kebun Teh')
+@section('title', 'Kebun Teh')
 
 @section('content')
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Desa Wisata Kayu Aro</title>
-  
-</head>
-<body>
-   
 
     <nav class="navbar navbar-expand-lg navbar-light bg-white py-3">
         <div class="container">
@@ -88,6 +78,4 @@
 
     {{-- Jika layout belum menyertakan JS, tambahkan; biasanya script berada di master --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    @endsection
-</body>
-</html>
+@endsection
--- resources/views/Desa Wisata/index2.blade.php
+++ resources/views/Desa Wisata/index2.blade.php
@@ -3,15 +3,6 @@
 @section('title', 'Halaman Home')
 
 @section('content')
-
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Desa Wisata Kayu Aro</title>
-
-<body>
  
     <nav class="navbar navbar-expand-lg navbar-light bg-white py-3">
         <div class="container">
@@ -78,8 +69,4 @@
     </main>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    @endsection
-</body>
-</html>
-
-
+@endsection
