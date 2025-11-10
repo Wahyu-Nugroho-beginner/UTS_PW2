9fdcb06c64b877172cd834466a6540cddc6569ce
Yozxora <albatinnopriayadi8899@gmail.com>
2025-11-10 21:38:23 +0700

    ngerapihin + bersih2'

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,6 @@ Route::get('/', function () {
     return view('Desa Wisata.index'); 
 });
 
-// Shortcut route to the alternate index page (index2)
 Route::get('/index2', function () {
     if (!session('user_id')) {
         return redirect()->route('login')->with('error', 'Silakan login terlebih dahulu');
@@ -17,10 +16,8 @@ Route::get('/index2', function () {
 })->name('home.index2');
 
 
-// Wisata pages
 Route::get('/wisata/{slug}', [WisataController::class, 'show'])->name('wisata.show');
 
-// Kalau ingin halaman kerinci tunggal, gunakan satu route (hapus duplikat)
 Route::get('/kerinci', function () {
     return view('Desa Wisata.Main.wisata.danau-kerinci');
 });
@@ -30,24 +27,19 @@ Route::post('/register', [WisataController::class, 'store'])->name('register.sto
 Route::get('/fasilitas/{slug}', [WisataController::class, 'showFasilitas'])->name('fasilitas.show');
 Route::get('/produk/store', [WisataController::class, 'showProduk'])->name('produk.showProduk');
 
-// Checkout Routes
 Route::get('/produk/checkout/{id}', [CheckoutController::class, 'index'])->name('produk.checkout');
 Route::post('/produk/checkout/process', [CheckoutController::class, 'process'])->name('produk.checkout.process');
 
-// Pesanan Routes
 Route::get('/pesanan', [CheckoutController::class, 'showPesanan'])->name('pesanan.show');
 
-// Authentication Routes
 Route::get('/login', [WisataController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [WisataController::class, 'login'])->name('login.store');
 Route::post('/logout', [WisataController::class, 'logout'])->name('logout');
 
-// Profile Routes
 Route::get('/profile', [WisataController::class, 'profile'])->name('profile.show');
 Route::get('/profile/edit', [WisataController::class, 'editProfile'])->name('profile.edit');
 Route::post('/profile/update', [WisataController::class, 'updateProfile'])->name('profile.update');
 
 Route::get('/pesanan', [PesananController::class, 'index'])->name('pesanan.list');
 
-// Menampilkan detail pesanan berdasarkan ID
 Route::get('/pesanan/{id}', [PesananController::class, 'show'])->name('pesanan.show');
