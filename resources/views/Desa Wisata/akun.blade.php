77afe3948b4475e3b7664f88edd0908e7edb25ec
Wahyu Nugroho <triwahyunugroho78@gmail.com>
2025-11-12 13:56:35 +0700

    benerin title di profile

--- resources/views/Desa Wisata/akun.blade.php
+++ resources/views/Desa Wisata/akun.blade.php
@@ -1,5 +1,7 @@
 @extends('desa wisata.layout.master')
 
+@section('title', 'Profile')
+
 @section('content')
 <div class="container my-5">
     <div class="row justify-content-center">
--- resources/views/Desa Wisata/edit-akun.blade.php
+++ resources/views/Desa Wisata/edit-akun.blade.php
@@ -1,5 +1,7 @@
 @extends('desa wisata.layout.master')
 
+@section('title', 'Edit Profile')
+
 @section('content')
 <div class="container my-5">
     <div class="row justify-content-center">
